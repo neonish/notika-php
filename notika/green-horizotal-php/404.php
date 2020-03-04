3fe5a7850361d7ab4c63c7eaeadc4edfab88d72f
neonish <christian.guss@gmail.com>
2020-03-04 18:10:09 +0100

    added dynamic-php version v0.1

---
+++ notika/green-horizotal-php/404.php
@@ -0,0 +1,12 @@
+    <!-- 404 Page area Start-->
+    <div class="error-page-area">
+        <div class="error-page-wrap">
+            <i class="notika-icon notika-close"></i>
+            <h2>ERROR <span class="counter">404</span></h2>
+            <p>Sorry, but the page you are looking for has note been found. Try checking the URL for an error, then hit the refresh button on your browser or try found something else in our app.</p>
+            <a href="<?php echo $global_url_dir; ?>" class="btn">Dashboard</a>
+            <!--<a href="#" class="btn error-btn-mg">Report Problem</a>-->
+        </div>
+    </div>
+    <!-- 404 Page area End-->
+    
